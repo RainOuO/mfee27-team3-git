f928f5ffa369484585f6c5af23a4999b1414f0df
Howard <howardchen1617@gmail.com>
2022-07-07 15:09:58 +0800

    template updated

--- ohdogcat_project/template/dashboard.php
+++ ohdogcat_project/template/dashboard.php
@@ -151,7 +151,7 @@
                     </div>
                     <hr class="flex-shrink-0">
                     <main id="main" class="content-main overflow-auto flex-shrink-1 h-100">
-                        <?=require('./main.php')?>
+                        <?php require('./main.php')?>
                     </main>
                 </div>
             </div>
