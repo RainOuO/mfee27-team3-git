e252f7433fe7176bccbffd57d5ab2e0bd4300f5c
Howard <howardchen1617@gmail.com>
2022-07-14 16:07:05 +0800

    0714

--- ohdogcat_project/edit-store-type/filter-section.php
+++
@@ -1,9 +0,0 @@
-<hr class="mt-0">
-
-<div class="d-flex bd-highlight mb-3 justify-content-end">
-                            <span class="iconpen">
-                                <img src="./7968880_pen_pen tool_adobe illustrator tool_icon.svg" alt="">
-                             </span>
-                            <a href="../store-info" class=" btn6 bd-highlight">商家資訊總覽</a>
-                            <!-- <a href="editpassword.php" class=" btn2 bd-highlight">變更新密碼</a> -->
-                        </div>
--- ohdogcat_project/test-account_setting.php
+++
@@ -1,3 +0,0 @@
-<?php
-$test_user = 6;
-?>
